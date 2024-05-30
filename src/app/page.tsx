2f8f820f61876651a2a5b66dd95e007a811a9247
Sourav Kumar Chakravartty <sourav.parshtechnologies@gmail.com>
2024-05-30 23:52:51 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,20 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>hiiii</div>
+    <>
+      <div className={styles.style}>helloo</div>
+
+      <table className={styles.table}>
+        <tr>
+          <th>no</th>
+          <th>name</th>
+          <th>number</th>
+        </tr>
+
+        <tr><td>1</td>
+        <td>sourav</td>
+        <td>90/100</td></tr>
+      </table>
+    </>
   );
 }
